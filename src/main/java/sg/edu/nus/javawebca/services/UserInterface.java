0a84b1d9c58d19d8dd0f85b5fbddf9d29408f766
yixinzhang2002 <zyx83028016zyx@gmail.com>
2024-06-19 14:01:25 +0800

    defined and implemented userInterface as a service for login

--- src/main/java/sg/edu/nus/javawebca/services/UserInterface.java
+++ src/main/java/sg/edu/nus/javawebca/services/UserInterface.java
@@ -1,4 +1,14 @@
 package sg.edu.nus.javawebca.services;
 
+import java.util.List;
+
+import sg.edu.nus.javawebca.models.User;
+
 public interface UserInterface {
+	  List<User> findAllUsers();
+	  User findUser(Integer userId);
+	  User createUser(User user);
+	  User changeUser(User user);
+	  void removeUser(User user);
+	  User authenticate(String username, String pwd);
 }
