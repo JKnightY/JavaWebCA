ff0ce7474036ec78cef67fff35f9aeaa93fef0b1
stewart <949257156@qq.com>
2024-06-19 17:22:48 +0800

    重构model

--- src/main/java/sg/edu/nus/javawebca/repositories/LeaveHistoryRepository.java
+++
@@ -1,7 +0,0 @@
-package sg.edu.nus.javawebca.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import sg.edu.nus.javawebca.models.LeaveHistory;
-
-public interface LeaveHistoryRepository extends JpaRepository<LeaveHistory, Integer> {
-}
