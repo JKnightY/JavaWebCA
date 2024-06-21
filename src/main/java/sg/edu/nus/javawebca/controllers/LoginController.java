0810fb810dc7e23463e625a15190119abbb76374
stewart <949257156@qq.com>
2024-06-21 10:47:03 +0800

    Merge branch 'refs/heads/cyl'
    # Conflicts:
    #	src/main/java/sg/edu/nus/javawebca/controllers/LoginController.java
    #	src/main/resources/application.properties

--- src/main/java/sg/edu/nus/javawebca/controllers/LoginController.java
+++ src/main/java/sg/edu/nus/javawebca/controllers/LoginController.java
@@ -7,53 +7,57 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import sg.edu.nus.javawebca.models.User;
 import sg.edu.nus.javawebca.services.UserInterface;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
 
 @Controller
 public class LoginController {
-  private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
-
   @Autowired
   private UserInterface userInterface;
 
   @GetMapping(value = {"/", "/login", "/home"})
   public String login(Model model) {
     model.addAttribute("user", new User());
+
     return "login";
   }
 
-  @PostMapping("/home/authenticate")
-  public String authenticate(@ModelAttribute("user") @Valid User user, BindingResult bindingResult, Model model, HttpSession session) {
+  @RequestMapping(value = "/home/authenticate")
+  public String authenticate(@ModelAttribute("user") @Valid User user, BindingResult bindingResult, Model model,
+                             HttpSession session) {
     if (bindingResult.hasErrors()) {
       return "login";
     }
 
-    logger.debug("Account: {}", user.getAccount());
-    logger.debug("Password: {}", user.getPassword());
+    User u = userInterface.authenticate(user.getAccount(), user.getPassword());
 
-    User authenticatedUser = userInterface.authenticate(user.getAccount(), user.getPassword());
-
-    if (authenticatedUser == null) {
+    if (u == null) {
       model.addAttribute("loginMessage", "Incorrect username/password");
       return "login";
+    } else {
+      session.setAttribute("user", u);
+
+      if (u.getRole() == 0) {
+        return "redirect:/Admin/users";
+      } else if (u.getRole() == 1) {
+        return "redirect:/staff/leaveApplication/history";
+      } else if (u.getRole() == 2) {
+        return "redirect:http://localhost:8080/manager/status/";
+      } else {
+        model.addAttribute("loginMessage", "Unauthorized access");
+        return "login";
+      }
     }
 
-    session.setAttribute("loggedInUser", authenticatedUser); // 保存用户信息到会话中
-
-    if (authenticatedUser.getRole() == 0) {
-      return "redirect:/Admin/users";
-    }
-
-    return "redirect:/staff/leaveApplication/history";
   }
 
-  @GetMapping("/logout")
+  @RequestMapping(value = "/logout")
   public String logout(HttpSession session) {
     session.invalidate();
     return "redirect:/login";
