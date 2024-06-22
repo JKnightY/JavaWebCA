beaf4d9698086d57e2ecaecd642d01a367749679
stewart <949257156@qq.com>
2024-06-22 14:04:40 +0800

    init data , add sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -14,8 +14,14 @@ ON DUPLICATE KEY UPDATE description=VALUES(description),
 
 -- Initialize leave_type data
 INSERT INTO leave_type (id, maxdays, name)
-VALUES (1, 14, 'AnnualLeave'),
-       (2, 60, 'MedicalLeave'),
-       (3, 100, 'CompensationLeave')
+VALUES (1, 14, 'Annual Leave (administrative employees)'),
+       (2, 18, 'Annual Leave (professional employees)'),
+       (3, 60, 'Medical Leave'),
+       (4, 100, 'Compensation Leave')
 ON DUPLICATE KEY UPDATE maxdays=VALUES(maxdays),
                         name=VALUES(name);
+
+-- Initialize users data with an admin account
+INSERT INTO users (id, account, password, username, department, role, email, leave_approverid, annual_leave_entitlement, annual_leave_entitlement_last, medical_leave_entitlement, medical_leave_entitlement_last, compensation_leave_balance, compensation_leave_balance_last) VALUES
+    (1, 'admin', 'admin', 'admin', 0, 0, 'admin@example.com', NULL, 0, 0, 0, 0, 0, 0)
+ON DUPLICATE KEY UPDATE account=VALUES(account), password=VALUES(password), role=VALUES(role), username=VALUES(username);
